440ee6bb3201da0e22ca4bd64a71ddecf94b1856
shahsag <shahsag@gmail.com>
2017-03-31 13:14:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!!!!!!')}
+  response.send('Hello World!!!!!!')
+)}
   
 app.get('/bcg', function(request, response) {
-response.send('Hello BCG!!!!!!')}
+  response.send('Hello BCG!!!!!!')
+)}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
