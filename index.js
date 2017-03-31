76644136ec6753eccf19b304dfa86183fd1aef1c
shahsag <shahsag@gmail.com>
2017-03-31 13:11:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello World!!!!!!')
+  
+app.get('/bcg', function(request, response) {
+response.send('Hello BCG!!!!!!')
 })
 
 app.listen(app.get('port'), function() {
