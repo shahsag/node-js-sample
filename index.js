9ab5a23be37915810f84e21d5095fb8d95fc6d23
shahsag <shahsag@gmail.com>
2017-03-31 13:15:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!!!!!!')
-}
+)}
   
 app.get('/bcg', function(request, response) {
   response.send('Hello BCG!!!!!!')
-}
+)}
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
